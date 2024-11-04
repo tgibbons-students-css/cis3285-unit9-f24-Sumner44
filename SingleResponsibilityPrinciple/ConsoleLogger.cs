cd0e8cbf507e0038130dcf68362cfad875ee8824
Sumner Cotton <scotton@css.edu>
2024-11-03 18:32:35 -0600

    Request 405 has been complete, and unit tests have been written to test simple trade vaildator

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,19 +1,34 @@
 ﻿using System;
-
+using System.IO;
 using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
 {
     public class ConsoleLogger : ILogger
     {
+        private const string LogFilePath = "log.xml";
+
         public void LogWarning(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("WARN: ", message), args);
+            LogMessage("WARN", message, args);
         }
 
         public void LogInfo(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("INFO: ", message), args);
+            LogMessage("INFO", message, args);
+        }
+
+        private void LogMessage(string type, string message, params object[] args)
+        {
+            // Format the message for console output
+            string formattedMessage = string.Format(message, args);
+            Console.WriteLine($"{type}: {formattedMessage}");
+
+            // Append log to XML file
+            using (StreamWriter logfile = File.AppendText(LogFilePath))
+            {
+                logfile.WriteLine($"<log><type>{type}</type><message>{formattedMessage}</message></log>");
+            }
         }
     }
 }
--- SingleResponsibilityPrincipleTests/SimpleTradeValidatorTests.cs
+++ SingleResponsibilityPrincipleTests/SimpleTradeValidatorTests.cs
@@ -10,131 +10,166 @@ namespace SingleResponsibilityPrinciple.Tests
         [TestMethod()]
         public void TestGoodCurrencyString()
         {
-            //Arrange
+            // Arrange
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
             string[] strData = { "AAABBB", "4444", "1.00" };
 
-            //Act
+            // Act
             bool result = tradeValidator.Validate(strData);
 
-            //Assert
+            // Assert
             Assert.IsTrue(result);
         }
 
         [TestMethod()]
         public void TestShortCurrencyString()
         {
-            //Arrange
+            // Arrange
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
             string[] strData = { "AAA", "4444", "1.00" };
 
-            //Act
+            // Act
             bool result = tradeValidator.Validate(strData);
 
-            //Assert
+            // Assert
             Assert.IsFalse(result);
         }
 
         [TestMethod()]
         public void TestLongCurrencyString()
         {
-            //Arrange
+            // Arrange
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
             string[] strData = { "AAAABBBB", "4444", "1.00" };
 
-            //Act
+            // Act
             bool result = tradeValidator.Validate(strData);
 
-            //Assert
+            // Assert
             Assert.IsFalse(result);
         }
 
         [TestMethod()]
         public void TestNormalTrade()
         {
-            //Arrange
+            // Arrange
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
             string[] strData = { "AAABBB", "4444", "1.00" };
 
-            //Act
+            // Act
             bool result = tradeValidator.Validate(strData);
 
-            //Assert
+            // Assert
             Assert.IsTrue(result);
         }
+
         [TestMethod()]
         public void TestAmount999()
         {
-            //Arrange
+            // Arrange
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
             string[] strData = { "AAABBB", "999", "1.00" };
 
-            //Act
+            // Act
             bool result = tradeValidator.Validate(strData);
 
-            //Assert
+            // Assert
             Assert.IsFalse(result);
         }
+
         [TestMethod()]
         public void TestAmount1000()
         {
-            //Arrange
+            // Arrange
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
             string[] strData = { "AAABBB", "1000", "1.00" };
 
-            //Act
+            // Act
             bool result = tradeValidator.Validate(strData);
 
-            //Assert
+            // Assert
             Assert.IsTrue(result);
         }
+
         [TestMethod()]
         public void TestAmountNeg10000()
         {
-            //Arrange
+            // Arrange
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
             string[] strData = { "AAABBB", "-10000", "1.00" };
 
-            //Act
+            // Act
             bool result = tradeValidator.Validate(strData);
 
-            //Assert
+            // Assert
             Assert.IsFalse(result);
         }
+
         [TestMethod()]
         public void TestAmount1m()
         {
-            //Arrange
+            // Arrange
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
             string[] strData = { "AAABBB", "1000000", "1.00" };
 
-            //Act
+            // Act
             bool result = tradeValidator.Validate(strData);
 
-            //Assert
+            // Assert
             Assert.IsTrue(result);
         }
+
         [TestMethod()]
         public void TestAmount101k()
         {
-            //Arrange
+            // Arrange
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
             string[] strData = { "AAABBB", "1000001", "1.00" };
 
-            //Act
+            // Act
             bool result = tradeValidator.Validate(strData);
 
-            //Assert
+            // Assert
             Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void TestAmount401k()
+        {
+            // Arrange
+            var logger = new ConsoleLogger();
+            var tradeValidator = new SimpleTradeValidator(logger);
+            string[] strData = { " ", "1000001", "1.00" };
+
+            // Act
+            bool result = tradeValidator.Validate(strData);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void TestEmptyCurrencyString()
+        {
+            // Arrange
+            var logger = new ConsoleLogger();
+            var tradeValidator = new SimpleTradeValidator(logger);
+            string[] strData = { "", "4444", "1.00" }; // First string is an empty string
+
+            // Act
+            bool result = tradeValidator.Validate(strData);
+
+            // Assert
+            Assert.IsFalse(result); // Assuming an empty currency string is invalid
         }
     }
 }
